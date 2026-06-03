625576ac75cf1da28eb2801784cf4efcf7a906b7
aditya1602 <aditya.p.sailesh@gmail.com>
2026-06-03 22:53:30 +0200

    Account creation and transfer

---
+++ ledger-api/src/main/java/com/adi/ledgerapi/controller/BankAccountController.java
@@ -0,0 +1,33 @@
+package com.adi.ledgerapi.controller;
+
+import com.adi.ledgerapi.dto.TransferRequest;
+import com.adi.ledgerapi.model.BankAccount;
+import com.adi.ledgerapi.service.BankAccountService;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class BankAccountController {
+
+    private final BankAccountService accountService;
+
+    public BankAccountController(BankAccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public BankAccount createAccount(@RequestBody BankAccount account) {
+        return accountService.createAccount(account);
+    }
+
+    @PostMapping("/transfers")
+    public String transferMoney(@RequestBody TransferRequest request) {
+        accountService.transferMoney(
+                request.getSourceAccountNumber(),
+                request.getDestinationAccountNumber(),
+                request.getAmount()
+        );
+        return "Transfer successful!";
+    }
+}
